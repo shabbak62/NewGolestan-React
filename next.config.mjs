c621631d3463f4d648b696766fc9d8337632e539
shabbak <shabbak62@gmail.com>
2026-09-23 09:08:45 +0330

    add github actions workflow for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "export",
+  basePath: "/NewGolestan-React",
+  images: { unoptimized: true },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
