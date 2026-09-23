f7bf850610551b32135c3fc72d00bd4f59fbcc58
shabbak <shabbak62@gmail.com>
2026-09-23 12:57:47 +0330

    update page and back to top

--- src/app/page.js
+++ src/app/page.js
@@ -82,7 +82,7 @@ export default function Home() {
     >
       <Header />
       <Hero />
-      <Up />
+      {/* <Up /> */}
       <Intro />
       <About />
       <div>
--- src/components/up/Up.jsx
+++ src/components/up/Up.jsx
@@ -1,62 +1,62 @@
-"use client";
+// "use client";
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-const UP = () => {
-  const [showButton, setShowButton] = useState(false);
+// const UP = () => {
+//   const [showButton, setShowButton] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 300) {
-        setShowButton(true);
-      } else {
-        setShowButton(false);
-      }
-    };
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       if (window.scrollY > 300) {
+//         setShowButton(true);
+//       } else {
+//         setShowButton(false);
+//       }
+//     };
 
-    window.addEventListener("scroll", handleScroll);
+//     window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
 
-  const backToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
+//   const backToTop = () => {
+//     window.scrollTo({
+//       top: 0,
+//       behavior: "smooth",
+//     });
+//   };
 
-  return (
-    <>
-      {showButton && (
-        <button
-          onClick={backToTop}
-          className="
-          z-50  
-          fixed
-            bottom-4 left-10
-            md:left-12.5
-            w-9 h-9
-            sm:w-10 sm:h-10
-            rounded-xl
-            bg-[#848f9bc4]
-            text-white
-            text-[20px]
-            sm:text-[24px]
-            flex items-center justify-center
-            shadow-2xl shadow-gray-600
-            transition-all duration-300
-            hover:bg-[#6f7a86]
-            hover:scale-110
-          "
-        >
-          ↑
-        </button>
-      )}
-    </>
-  );
-};
+//   return (
+//     <>
+//       {showButton && (
+//         <button
+//           onClick={backToTop}
+//           className="
+//           z-50  
+//           fixed
+//             bottom-4 left-10
+//             md:left-12.5
+//             w-9 h-9
+//             sm:w-10 sm:h-10
+//             rounded-xl
+//             bg-[#848f9bc4]
+//             text-white
+//             text-[20px]
+//             sm:text-[24px]
+//             flex items-center justify-center
+//             shadow-2xl shadow-gray-600
+//             transition-all duration-300
+//             hover:bg-[#6f7a86]
+//             hover:scale-110
+//           "
+//         >
+//           ↑
+//         </button>
+//       )}
+//     </>
+//   );
+// };
 
-export default UP;
+// export default UP;
