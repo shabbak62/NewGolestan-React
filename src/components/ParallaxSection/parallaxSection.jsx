88694c1d0bc1d8686eb4ac9ebe65b9079de2982b
shabbak <shabbak62@gmail.com>
2026-09-24 07:39:24 +0330

    find-bug-scrool

--- src/components/ParallaxSection/parallaxSection.jsx
+++ src/components/ParallaxSection/parallaxSection.jsx
@@ -1,7 +1,12 @@
+import { basePath } from "@/lib/basePath";
 const parallaxSection = () => {
   return (
     <div>
-      <div className="w-full h-75 bg-transparent "></div>
+      <div className="md:w-full h-75 md:bg-transparent hidden"></div>
+      <div
+        className="md:hidden w-full h-75"
+        style={{ backgroundImage: `url(${basePath}/Image/recipe-baner-1.png)` }}
+      ></div>
     </div>
   );
 };
